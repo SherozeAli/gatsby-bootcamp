32f8e22a0436c9afb466956de724b628f471611f
SherozeAli <sherozeali777@gmail.com>
2020-04-08 21:22:03 +0500

    start GQL with site siteMetadata author title name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
-
 module.exports = {
+
+  siteMetadata:{
+    title:'FULL-STACK bootcamp',
+    author:'SHEROZE ALI'
+  },
   plugins: [`gatsby-plugin-sass`]
 }
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,14 +1,26 @@
 import React from "react"
 import './footer.scss'
+import {useStaticQuery,graphql} from 'gatsby'
 const Footer =()=>{
+const data=useStaticQuery(graphql`
+query{
+    site{
+        siteMetadata{
+            author
+            title
+        }
+    }
+}
 
+
+`)
     return(
 
         <div>
             <h1>
             Footer
             </h1>
-            <p>Created by SHEROZE ALI </p>
+            <p>Created by { data.site.siteMetadata.author} </p>
         </div>
     )
 }
